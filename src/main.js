58058590a22d59f8083035d292b62a76e28b4992
seylu <98249191+seyLu@users.noreply.github.com>
2024-09-19 07:32:30 +0800

    feat: add datepicker

--- src/main.js
+++ src/main.js
@@ -2,6 +2,55 @@ const LIFESPAN = 89;
 const WEEKS_IN_YEAR = 52;
 
 document.addEventListener('DOMContentLoaded', function () {
+    new AirDatepicker('#datepicker', {
+        locale: {
+            days: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+            ],
+            daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            months: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December',
+            ],
+            monthsShort: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec',
+            ],
+            today: 'Today',
+            clear: 'Clear',
+            dateFormat: 'MM/dd/yyyy',
+            timeFormat: 'hh:mm aa',
+            firstDay: 0,
+        },
+    });
+
     const canvas = document.getElementById('canvas');
 
     for (let age = -1; age <= LIFESPAN; age++) {
